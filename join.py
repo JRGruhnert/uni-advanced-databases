7e68af175f366d84ec35cd685886249b23956486
Jan Gruhnert <jan-gruhnert@gmx.de>
2023-07-24 02:09:36 +0200

    Add code

--- join.py
+++ join.py
@@ -2,38 +2,38 @@ from collections import defaultdict
 from parallel_sort import merge_sort_threaded
 
 # Hash-Join implementation
-def hash_join(table1, table2, join_column):
+def hash_join(table1, table2, join_column1, join_column2):
     # Create hash table for table1 using the join_column
     hash_table = defaultdict(list)
     # hash phase
     for row in table1:
-        hash_table[row[join_column]].append(row)
+        hash_table[row[join_column1]].append(row)
 
     # Perform the join
-    return [(row1, row2) for row2 in table2 for row1 in hash_table[row2[join_column]]]
+    return [(row1, row2) for row2 in table2 for row1 in hash_table[row2[join_column2]]]
 
 # Sort-Merge-Join implementation
-def sort_merge_join(parallel: bool, table1, table2, join_column):
+def sort_merge_join(parallel: bool, table1, table2, join_column1, join_column2):
     # Sort both tables based on the join_column
     sorted_table1 = []
     sorted_table2 = []
     
     if parallel:
-        sorted_table1 = merge_sort_threaded(table1, join_column)
-        sorted_table2 = merge_sort_threaded(table2, join_column)
+        sorted_table1 = merge_sort_threaded(table1, join_column1)
+        sorted_table2 = merge_sort_threaded(table2, join_column2)
     else:
-        sorted_table1 = sorted(table1, key=lambda x: x[join_column])
-        sorted_table2 = sorted(table2, key=lambda x: x[join_column])
+        sorted_table1 = sorted(table1, key=lambda x: x[join_column1])
+        sorted_table2 = sorted(table2, key=lambda x: x[join_column2])
 
     # Perform the join
     result = []
     i, j = 0, 0
     while i < len(sorted_table1) and j < len(sorted_table2):
-        if sorted_table1[i][join_column] == sorted_table2[j][join_column]:
+        if sorted_table1[i][join_column1] == sorted_table2[j][join_column2]:
             # Combine the rows from both tables excluding the join_column
-            result.append({**sorted_table1[i], **{k: v for k, v in sorted_table2[j].items() if k != join_column}})
+            result.append({**sorted_table1[i], **{k: v for k, v in sorted_table2[j].items() if k != join_column2}})
             j += 1
-        elif sorted_table1[i][join_column] < sorted_table2[j][join_column]:
+        elif sorted_table1[i][join_column1] < sorted_table2[j][join_column2]:
             i += 1
         else:
             j += 1
--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import time
 from data import preprocess_data
 from join import hash_join, sort_merge_join
 
-FILE_PATH = "/home/jangruhnert/Documents/watdiv.10M/watdiv.10M.nt"
+FILE_PATH = "/home/jangruhnert/Downloads/watdiv.10M/watdiv.10M.nt"
+JOIN_COLUMN1 = 'Object'  # Assuming 'Object' column is used for joining
+JOIN_COLUMN2 = 'Subject'
+FOLLOWS = rdflib.term.URIRef('http://db.uwaterloo.ca/~galuc/wsdbm/follows')
+FRIEND_OF = rdflib.term.URIRef('http://db.uwaterloo.ca/~galuc/wsdbm/friendOf')
+LIKES = rdflib.term.URIRef('http://db.uwaterloo.ca/~galuc/wsdbm/likes')
+HAS_REVIEW = rdflib.term.URIRef('http://db.uwaterloo.ca/~galuc/wsdbm/hasReview')
 
 # Measure execution time for a function
 # Wraps the function and returns the result and the execution time
@@ -14,23 +20,32 @@ def measure_execution_time(func, *args):
     execution_time = end_time - start_time
     return result, execution_time
 
-join_column = 'Object'  # Assuming 'Object' column is used for joining
-follows = rdflib.term.URIRef('http://db.uwaterloo.ca/~galuc/wsdbm/follows')
-friendOf = rdflib.term.URIRef('http://db.uwaterloo.ca/~galuc/wsdbm/friendOf')
+def request_with_hash_join(property_tables, join_column1, join_column2):
+    join = hash_join(property_tables[FOLLOWS], property_tables[FRIEND_OF], JOIN_COLUMN1, JOIN_COLUMN2)
+    join = hash_join(join, property_tables[LIKES], JOIN_COLUMN1, JOIN_COLUMN2)
+    return hash_join(join, property_tables[HAS_REVIEW], JOIN_COLUMN1, JOIN_COLUMN2)
+
+def request_with_sort_merge_join(parallel, property_tables, join_column1, join_column2):
+    join = sort_merge_join(parallel, property_tables[FOLLOWS], property_tables[FRIEND_OF], JOIN_COLUMN1, JOIN_COLUMN2)
+    join = sort_merge_join(parallel, join, property_tables[LIKES], JOIN_COLUMN1, JOIN_COLUMN2)
+    return sort_merge_join(parallel, join, property_tables[HAS_REVIEW], JOIN_COLUMN1, JOIN_COLUMN2)
+
 string_dict, property_tables = preprocess_data(FILE_PATH)
 print("Data preprocessed")
 
-result_hash_join, time_hash_join = measure_execution_time(hash_join, property_tables[follows], property_tables[friendOf], join_column)
+
+
+result_hash_join, time_hash_join = measure_execution_time(request_with_hash_join, property_tables, JOIN_COLUMN1, JOIN_COLUMN2)
 result_hash_join = None
 print("Time hash Join finished!")
 print("Time for time hash join: " + str(time_hash_join))
 
-result_sort_merge_join, time_sort_merge_join = measure_execution_time(sort_merge_join, False, property_tables[follows], property_tables[friendOf], join_column)
+result_sort_merge_join, time_sort_merge_join = measure_execution_time(request_with_sort_merge_join, False, property_tables, JOIN_COLUMN1, JOIN_COLUMN2)
 result_sort_merge_join = None
 print("Sort merge join finished!")
 print("Time for sort merge join: " + str(time_sort_merge_join))
 
-result_sort_merge_parallel_join, time_sort_merge_parallel_join = measure_execution_time(sort_merge_join, True, property_tables[follows], property_tables[friendOf], join_column)
+result_sort_merge_parallel_join, time_sort_merge_parallel_join = measure_execution_time(request_with_sort_merge_join, True, property_tables, JOIN_COLUMN1, JOIN_COLUMN2)
 result_sort_merge_parallel_join = None
 print("Sort merge join parallel finished!")
 print("Time for sort merge parallel join: " + str(time_sort_merge_parallel_join))
